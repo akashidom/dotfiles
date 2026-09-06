cc075275f1b0aa47395cb6d3593d136790c694cc
akashidom <akashidomon@gmail.com>
2026-09-07 00:00:57 +0800

    update caelestia config

---
+++ dot_config/caelestia/hypr-user.lua
@@ -0,0 +1,9 @@
+local vars = require("variables")
+
+hl.config({
+    input = {
+        touchpad = {
+            natural_scroll = vars.naturalScroll,
+        }
+    }
+})
--- dot_config/caelestia/hypr-vars.lua
+++ dot_config/caelestia/hypr-vars.lua
@@ -1,8 +1,134 @@
+local scheme = require("scheme.current")
+
 return {
+    -- Apps
+    terminal                   = "foot",
+    browser                    = "zen-browser",
+    editor                     = "zeditor",
+    fileExplorer               = "thunar",
+    audioSettings              = "pwvucontrol",
+
+    -- Touchpad
+    touchpadDisableTyping      = false,
+    gestureFingers             = 3,
+    workspaceSwipeFingers      = 4,
+    gestureFingersMore         = 4,
+
     -- Blur
     blurEnabled                = true,
     blurSpecialWs              = false,
-    blurPopups                 = false,
-    blurInputMethods           = false,
-    blurXray                   = false,
+    blurPopups                 = true,
+    blurInputMethods           = true,
+    blurSize                   = 8,
+    blurPasses                 = 2,
+    blurXray                   = true,
+
+    -- Shadow
+    shadowEnabled              = true,
+    shadowRange                = 15,
+    shadowRenderPower          = 4,
+    shadowColour               = "rgba(" .. scheme.inversePrimary .. "10)",
+
+    -- Gaps
+    workspaceGaps              = 20,
+    windowGapsIn               = 5,
+    windowGapsOut              = 10,
+    singleWindowGapsOut        = 20,
+
+    -- Window styling
+    windowOpacity              = 0.95,
+    windowRounding             = 15,
+    windowBorderSize           = 2,
+    activeWindowBorderColour   = "rgba(" .. scheme.primary .. "e6)",
+    inactiveWindowBorderColour = "rgba(" .. scheme.onSurfaceVariant .. "11)",
+
+    -- Misc
+    volumeStep                 = 10,
+    cursorTheme                = "sweet-cursors",
+    cursorSize                 = 24,
+    sleepGestureCmd            = "systemctl suspend-then-hibernate",
+
+    -- Workspaces
+    kbMoveWinToWs              = "ALT + SUPER",
+    kbMoveWinToWsGroup         = "CTRL + ALT + SUPER",
+    kbGoToWs                   = "SUPER",
+    kbGoToWsGroup              = "CTRL + SUPER",
+    kbNextWs                   = { "SUPER + mouse_down", "CTRL + SUPER + Right", "SUPER + Page_Down" },
+    kbPrevWs                   = { "SUPER + mouse_up", "CTRL + SUPER + Left", "SUPER + Page_Up" },
+
+    -- Window Group
+    kbWindowGroupCycleNext     = "CTRL + ALT + TAB",
+    kbWindowGroupCyclePrev     = "CTRL + SHIFT + ALT + TAB",
+    kbUngroup                  = "SUPER + U",
+    kbToggleGroup              = "SUPER + Comma",
+
+    -- Window Action
+    kbMoveWindow               = "SUPER + Z",
+    kbResizeWindow             = "SUPER + X",
+    kbWindowPip                = "SUPER + ALT + Backslash",
+    kbPinWindow                = "SUPER + P",
+    kbWindowFullscreen         = "SUPER + F",
+    kbWindowBorderedFullscreen = "SUPER + ALT + F",
+    kbToggleWindowFloating     = "SUPER + ALT + Space",
+    kbCloseWindow              = "SUPER + Q",
+
+    -- Special workspaces toggles
+    kbSpecialWs                = "SUPER + S",
+    kbSystemMonitorWs          = "CTRL + SHIFT + Escape",
+    kbMusicWs                  = "SUPER + M",
+    kbCommunicationWs          = "SUPER + D",
+    kbTodoWs                   = "SUPER + R",
+
+    -- Apps (Keybinds)
+    kbTerminal                 = "SUPER + T",
+    kbBrowser                  = "SUPER + W",
+    kbEditor                   = "SUPER + C",
+    kbFileExplorer             = "SUPER + E",
+
+    -- Misc (Keybinds)
+    kbSession                  = "CTRL + ALT + Delete",
+    kbShowSidebar              = "SUPER + N",
+    kbClearNotifs              = "CTRL + ALT + C",
+    kbShowPanels               = "SUPER + K",
+    kbLock                     = "SUPER + L",
+    kbRestoreLock              = "SUPER + ALT + L",
+
+    -- Custom
+    naturalScroll              = false,
+    touchpadScrollFactor       = 1.5,
+    volumeMax                  = 130,
+    kbMoveWinToWsSpecial       = { "SUPER + ALT + S", "CTRL + SUPER + SHIFT + Up" },
+    kbMoveWinFromWsSpecial     = "CTRL + SUPER + SHIFT + Down",
+    kbMoveWinToWsNext          = { "SUPER + ALT + mouse_down", "SUPER + ALT + Page_Down", "CTRL + SUPER + SHIFT + Right" },
+    kbMoveWinToWsPrev          = { "SUPER + ALT + mouse_up", "SUPER + ALT + Page_Up", "CTRL + SUPER + SHIFT + Left" },
+    kbNextWsGroup              = "CTRL + SUPER + mouse_down",
+    kbPrevWsGroup              = "CTRL + SUPER + mouse_up",
+    kbWindowCycleNext          = "ALT + TAB",
+    kbWindowCyclePrev          = "SHIFT + ALT + TAB",
+    kbGroupLockActive          = "SUPER + SHIFT + Comma",
+    kbWindowDecreaseWidth      = { "SUPER + Minus", "SUPER + ALT + Left" },
+    kbWindowIncreaseWidth      = { "SUPER + Equal", "SUPER + ALT + Right" },
+    kbWindowDecreaseHeight     = { "SUPER + SHIFT + Minus", "SUPER + ALT + Up" },
+    kbWindowIncreaseHeight     = { "SUPER + SHIFT + Equal", "SUPER + ALT + Down" },
+    kbCenterWindow             = "CTRL + SUPER + Backslash",
+    kbNormalizeWindow          = "CTRL + SUPER + ALT + Backslash",
+    kbAudioSettings            = "CTRL + ALT + V",
+    kbScreenshot               = "Print",
+    kbScreenshotFreeze         = "SUPER + SHIFT + S",
+    kbScreenshotRegion         = "SUPER + SHIFT + ALT + S",
+    kbRecord                   = "CTRL + ALT + R",
+    kbRecordSound              = "SUPER + ALT + R",
+    kbRecordRegion             = "SUPER + SHIFT + ALT + R",
+    kbColorPicker              = "SUPER + SHIFT + C",
+    kbMediaToggle              = "CTRL + SUPER + Space",
+    kbMediaNext                = "CTRL + SUPER + Equal",
+    kbMediaPrev                = "CTRL + SUPER + Minus",
+    kbMediaStop                = "CTRL + SUPER + Backspace",
+    kbVolumeMute               = "SUPER + SHIFT + M",
+    kbLauncher                 = "SUPER + SUPER_L",
+    kbSleep                    = "SUPER + SHIFT + L",
+    kbClipboard                = "SUPER + V",
+    kbClipboardDel             = "SUPER + ALT + V",
+    kbClipboardPasteLatest     = "CTRL + SHIFT + ALT + V",
+    kbEmoji                    = "SUPER + Period",
 }
